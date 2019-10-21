7c055a81de098577dce725a1edf8d36bd8b3db49
KingsleyUsoroeno <kingsleyenou@gmail.com>
2019-10-21 13:20:06 +0100

    ft-integrated mongoDb for database queries

--- src/main/java/io/springbootstarter/student/entity/Student.java
+++ src/main/java/io/springbootstarter/student/entity/Student.java
@@ -1,19 +1,30 @@
 package io.springbootstarter.student.entity;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+@Document("student") // Marks these Class to a table that needs to be stored/persisted into our Database
 public class Student {
 
+    @Id
     private int id;
     private String name;
     private String course;
+
+    @Indexed(unique = true)
+    private String parentsEmail;
 
     public Student() {
 
     }
 
-    public Student(int id, String name, String course) {
+    public Student(int id, String name, String course, String parentsEmail) {
         this.id = id;
         this.name = name;
         this.course = course;
+        this.parentsEmail = parentsEmail;
     }
 
     public int getId() {
@@ -38,5 +49,13 @@ public class Student {
 
     public void setCourse(String course) {
         this.course = course;
+    }
+
+    public String getParentsEmail() {
+        return parentsEmail;
+    }
+
+    public void setParentsEmail(String parentsEmail) {
+        this.parentsEmail = parentsEmail;
     }
 }
--- src/main/java/io/springbootstarter/student/service/StudentService.java
+++ src/main/java/io/springbootstarter/student/service/StudentService.java
@@ -2,6 +2,7 @@ package io.springbootstarter.student.service;
 
 import io.springbootstarter.student.dao.StudentDao;
 import io.springbootstarter.student.entity.Student;
+import io.springbootstarter.student.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,22 +15,30 @@ public class StudentService {
     private StudentDao studentDao;
 
     public Collection<Student> getAllStudents() {
-        return studentDao.getAllStudents().values();
+        return studentDao.findAll();
     }
 
     public Student getStudentById(int id) {
-        return studentDao.getStudentById(id);
+        return studentDao.findById(id).orElseThrow(ResourceNotFoundException::new);
+    }
+
+    public Student findStudentByParentsEmail(String email) {
+        return studentDao.findByParentsEmail(email).orElseThrow(ResourceNotFoundException::new);
     }
 
     public void createStudent(Student student) {
-        studentDao.createStudent(student);
+        studentDao.save(student);
     }
 
-    public void updateStudent(Student student) {
-        studentDao.updateStudent(student);
+    public Student updateStudent(int id, Student newStudent) {
+        Student student = studentDao.findById(id).orElseThrow(ResourceNotFoundException::new);
+        student.setName(newStudent.getName());
+        student.setCourse(newStudent.getCourse());
+        student.setParentsEmail(newStudent.getParentsEmail());
+        return studentDao.save(student);
     }
 
     public void deleteStudent(int id) {
-        studentDao.deleteStudent(id);
+        studentDao.deleteById(id);
     }
 }
